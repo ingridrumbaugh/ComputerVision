7cfe8cac171f4f997d4cbc14a77b8a2e21e90c7e
Ingrid Rumbaugh <rumbaugi@lafayette.edu>
2018-02-22 13:42:57 -0500

    More examples up to ch 6

---
+++ imutils.py
@@ -0,0 +1,24 @@
+'''
+This file will store basic image processing methods,
+allowing us to conveniently call them without writing
+a lot of code. 
+'''
+import numpy as np
+import cv2 
+
+# Define a translate function 
+def translate(image, x, y):
+    M = np.float32([[1, 0, x], [0, 1, y]])
+    shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+    return shifted 
+
+def rotate(image, angle, center = None, scale = 1,0):
+    (h, w) = image.shape[:2]
+    
+    if center is None:
+        center = (w/2, h/2)
+        
+    M = cv2.getRotationMatrix2D(center, angle, scale)
+    rotated = cv2.warpAffine(image, M, (w, h)) 
+    
+    return rotated 
