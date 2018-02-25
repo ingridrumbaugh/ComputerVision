e5f8999ed037dc7060a583de5dcdf8fba95a788b
Ingrid Rumbaugh <rumbaugi@lafayette.edu>
2018-02-25 17:07:39 -0500

    Finished Ch 11

--- counting_coins.py
+++ counting_coins.py
@@ -29,7 +29,7 @@ cv2.imshow("Edges", edged)
 # Type of contours: RETR_EXTERNAL, retrieve only outermost contours
 # All contours would be: RETR_LIST 
 # How to approx contour: CHAIN_APPROX_SIMPLE 
-(cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+(_, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
 
 print "There are %d coins in this image" % (len(cnts)) 
 
@@ -37,3 +37,36 @@ coins = image.copy()
 cv2.drawContours(coins, cnts, -1, (0, 255, 0), 2) 
 cv2.imshow("Coins", coins) 
 cv2.waitKey(0) 
+
+# Crop each individual coin from the image 
+# Below line 25 
+
+# Loop over the contours
+for (i, c) in enumerate(cnts):
+    # Finds the "enclosing box" that the contour fits into
+    (x, y, w, h) = cv2.boundingRect(c) 
+    
+    print "Coin # %d" % (i+1) 
+    # Crop the coin from the image using bounding box coords 
+    coin = image[y:y+h,x:x+w]
+    cv2.imshow("Coin", coin) 
+    # Initialize mask 
+    mask = np.zeros(image.shape[:2], dtype = "uint8")
+    # Fit a circle to the contour 
+    ((centerX, centerY), radius) = cv2.minEnclosingCircle(c)
+    cv2.circle(mask, (int(centerX), int(centerY)), int(radius), 255, -1)
+    mask = mask[y:y+h, x:x+w]
+    
+    cv2.imshow("Masked Coin", cv2.bitwise_and(coin, coin, mask = mask))
+    cv2.waitKey(0) 
+
+
+
+
+
+
+
+
+
+
+
