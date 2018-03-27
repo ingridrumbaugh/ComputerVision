5bb12e7fb810e0eb57c9b680fb1e87dec15a5562
Ingrid Rumbaugh <rumbaugi@lafayette.edu>
2018-03-27 00:53:41 -0400

    Notes on CV_BRIDGE Tutorial

--- testing_npHistogram.py
+++ testing_npHistogram.py
@@ -31,14 +31,23 @@ edged = cv2.Canny(blurred, 30, 150)
 # split ground truth image into channels 
 ogchans = cv2.split(croparchfish)
 colors = ('b', 'g', 'r') 
+
+plt.figure()
+plt.title("'Flattened' Color Histogram")
+plt.xlabel("Bins")
+plt.ylabel("# of Pixels") 
+
 # Loop over each of  the channels in the image 
 # For each channel compute a histogram 
 for (chan, color) in zip(ogchans, colors):
     #                     channels, mask, size, ranges,
     oghist = cv2.calcHist([chan], [0], None, [256], [0, 256])
-    gt_hist, gt_bins = np.histogram(croparchfish, bins = 100, range = [0,256])
-    plt.plot(oghist, color = color, linewidth = 2.0) 
+    gt_hist, gt_bins = np.histogramdd(croparchfish, bins = 100, range = [0,256])
+    plt.plot(gt_hist, color = color, linewidth = 2.0) 
     plt.xlim([0, 256]) 
+
+plt.show()
+cv2.waitKey(0) 
 
 hist_height = 64
 hist_width  = 256
